a646040a05168dc3be93e32a20cce1af9a326e8a
Gabriel Costa <gubsocosta@gmail.com>
2024-11-24 15:23:33 -0300

    feature(todo): create todo service

--- src/views/todo/TodoList.vue
+++ src/views/todo/TodoList.vue
@@ -1,3 +1,30 @@
 <template>
   <h1>Todo List</h1>
+  <ul>
+    <li v-for="todo in todoList" :key="todo.id">
+      {{ todo.title }}
+    </li>
+    <input type="text" name="name" v-model="name"/>
+  </ul>
 </template>
+
+<script>
+
+import {onMounted, ref} from "vue";
+import TodoService from "@/services/todo.service";
+
+export default {
+  name: 'TodoList',
+  setup() {
+    const todoList = ref([]);
+
+    onMounted(() => {
+      TodoService.getAll()
+        .then((response) => todoList.value = response.data)
+        .catch((err) => console.log(err));
+    });
+
+    return {todoList}
+  }
+}
+</script>
